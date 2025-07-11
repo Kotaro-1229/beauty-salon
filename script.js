6e18df3bd8c3ae3b497c75749b95479bd6b6c99d
Kotaro-Hayashi <kotaro031229@gmail.com>
2025-07-11 15:07:10 +0900

    色々追加

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+//ハンバーガーメニュー
+const menuOpen=document.querySelector('#menu-open')
+const menuClose=document.querySelector('#menu-close')
+const menuPanel=document.querySelector('#menu-panel')
+//ハンバーガーメニューを開く
+const menuOptions={
+  duration:1400,
+  easing: 'ease',
+  fill:'forwards',
+};
+menuOpen.addEventListener('click',()=>{
+  menuPanel.animate({translate:['100vw',0]},menuOptions);
+});
+
+menuClose.addEventListener('click',()=>{
+  menuPanel.animate({translate:[0,'100vw']},menuOptions);
+});
+
 //スライダー設置
 const swiper = new Swiper('.swiper', {
   // Optional parameters
